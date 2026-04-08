1484757ccd1a5d4a406ce50716572e197fd50f64
Aryan Marthak <aryan.marthak1@gmail.com>
2026-04-08 22:41:18 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,9 +6,13 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     conn, _ = server.accept() # wait for client
     data = conn.recv(1024)
-    conn.sendall(b"\x00\x00\x00\x00" + data[8:12])
-    
-
+    version = int.from_bytes(data[6:8], "big")
+    if version <= 4:
+        error_code = 0
+    else:
+        error_code = 35
+    error_bytes = error_code.to_bytes(2, "big")
+    conn.sendall(b"\x00\x00\x00\x00" + data[8:12] + error_bytes)
 
 if __name__ == "__main__":
     main()
