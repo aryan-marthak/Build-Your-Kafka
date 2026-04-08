7b4dca6675d32e6d98996645df6f06d8c53e5de4
Aryan Marthak <aryan.marthak1@gmail.com>
2026-04-08 19:13:22 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import socket  # noqa: F401
 def main():
     print("Logs from your program will appear here!")
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    conn = server.accept() # wait for client
+    conn.sendall(b"\x00\x00\x00\x00\x00\x00\x00\x07")
 
 
 if __name__ == "__main__":
