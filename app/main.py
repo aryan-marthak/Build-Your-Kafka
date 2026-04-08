fa23260f254f423368153456051cf82741fa5575
Aryan Marthak <aryan.marthak1@gmail.com>
2026-04-08 19:49:35 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def main():
     print("Logs from your program will appear here!")
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     conn, _ = server.accept() # wait for client
-    conn.sendall(b"\x00\x00\x00\x00\x00\x00\x00\x07")
+    data = conn.recv(1024)
+    conn.sendall(b"\x00\x00\x00\x00" + data[8:12])
+    
 
 
 if __name__ == "__main__":
